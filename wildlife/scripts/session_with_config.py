4b77d233117b3f6a67349423f50c2795263e92c0
Philipp Sadler <philipp.sadler@gmail.com>
2019-06-09 16:52:16 +0200

    add split file option for evaluation

--- wildlife/scripts/session_with_config.py
+++ wildlife/scripts/session_with_config.py
@@ -20,6 +20,7 @@ def main():
     parser.add_argument("-c", "--configuration", help="Determine a specific configuration to use. If not specified, the default is used.")
     parser.add_argument("-f", "--path_to_model", help="The absolute path to the model to predict or continue training.")
     parser.add_argument("-s", "--split_name", help="""The split name to perform the prediction or training on. This is required for predict. For example 'wl-c11'.""")
+    parser.add_argument("-sf", "--split_files", help="A whitespace separated list of file names. For wildlife training defaults to [target_train, target_dev, target_test]")
     parser.add_argument("-m", "--do_multiclass", action="store_true", default=False)
     parser.add_argument("-d", "--dryrun", action="store_true")
     
@@ -53,7 +54,10 @@ def main():
         if not run_opts.path_to_model:
             raise Exception("Please provide the path to the model using the '-f' option and retry.")
         if model_type == "baseline":
-            start_evaluate_baseline(run_opts.path_to_model, dataset_dir, split_name, do_multiclass=run_opts.do_multiclass)
+            split_file_test = "target_test"
+            if run_opts.split_files:
+                split_file_test = run_opts.split_files.split(" ")[0] # expect only a single file here for now
+            start_evaluate_baseline(run_opts.path_to_model, dataset_dir, split_name, split_file_test, do_multiclass=run_opts.do_multiclass)
             
 if __name__ == '__main__':
     main()
